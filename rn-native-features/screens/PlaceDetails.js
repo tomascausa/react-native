f4337e8cbe68e928819d6fe40dbad988ba7c1d33
tcausa-utilitynyc <tom.causa@utilitynyc.com>
2022-05-18 22:21:04 -0300

    Places app

---
+++ rn-native-features/screens/PlaceDetails.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
+import { Colors } from '../constants/colors';
+import { fetchPlaceDetails } from '../util/database';
+import OutlinedButton from '../components/UI/OutlinedButton';
+
+const PlaceDetails = ({ route, navigation }) => {
+    const [fetchedPlace, setFetchedPlace] = useState();
+
+    const showOnMapHandler = () => {
+        navigation.navigate('Map', {
+            initialLat: fetchedPlace.location.lat,
+            initialLng: fetchedPlace.location.lng,
+        });
+    }
+
+    const selectedPlaceId = route.params.placeId;
+
+    useEffect(() => {
+        const loadPlaceData = async () => {
+            const place = await fetchPlaceDetails(selectedPlaceId);
+            setFetchedPlace(place);
+            navigation.setOptions({
+                title: place.title,
+            });
+        }
+
+        loadPlaceData();
+    }, [selectedPlaceId]);
+
+    if (!fetchedPlace) {
+        return (
+            <View style={styles.fallback}>
+                <Text>Loading place data...</Text>
+            </View>
+        );
+    }
+
+    return (
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
+            <View style={styles.locationContainer}>
+                <View style={styles.addressContainer}>
+                    <Text style={styles.address}>{fetchedPlace.address}</Text>
+                </View>
+                <OutlinedButton icon="map" onPress={showOnMapHandler}>
+                    View on Map
+                </OutlinedButton>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    fallback: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        height: '35%',
+        minHeight: 300,
+        width: '100%',
+    },
+    locationContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    addressContainer: {
+        padding: 20,
+    },
+    address: {
+        color: Colors.primary500,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
+
+export default PlaceDetails;
